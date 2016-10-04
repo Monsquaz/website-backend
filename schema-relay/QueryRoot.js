a0c2cd87ea1f70115eafdd9dcec13a88a8b573b6
Andrew Carlson <acarl005@g.ucla.edu>
2016-10-04 04:21:50 -0700

    add relay example

---
+++ schema-relay/QueryRoot.js
@@ -0,0 +1,55 @@
+import {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLString,
+  GraphQLInt
+} from 'graphql'
+
+import knex from './database'
+import joinMonster from 'join-monster'
+import User from './User'
+import { getNode } from './Node'
+
+export default new GraphQLObjectType({
+  description: 'global query object',
+  name: 'Query',
+  fields: () => ({
+    version: {
+      type: GraphQLString,
+      resolve: () => joinMonster.version
+    },
+    node: getNode().nodeField,
+    users: {
+      type: new GraphQLList(User),
+      resolve: (parent, args, context, ast) => {
+        return joinMonster(ast, context, sql => {
+          // place the SQL query in the response headers. ONLY for debugging. Don't do this in production
+          if (context) {
+            context.set('X-SQL-Preview', sql.replace(/\n/g, '%0A'))
+          }
+          return knex.raw(sql)
+        })
+      }
+    },
+    user: {
+      type: User,
+      args: {
+        id: {
+          description: 'The users ID number',
+          type: GraphQLInt
+        }
+      },
+      where: (usersTable, args, context) => { // eslint-disable-line no-unused-vars
+        if (args.id) return `${usersTable}.id = ${args.id}`
+      },
+      resolve: (parent, args, context, ast) => {
+        return joinMonster(ast, context, sql => {
+          if (context) {
+            context.set('X-SQL-Preview', sql.replace(/\n/g, '%0A'))
+          }
+          return knex.raw(sql)
+        })
+      }
+    }
+  })
+})
