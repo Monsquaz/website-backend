cbaf1468a1246d0a0da3eb8f3fc7063550074535
Andrew Carlson <acarl005@g.ucla.edu>
2016-10-20 20:08:26 -0700

    log the database connection for debugging

--- schema-relay/database.js
+++ schema-relay/database.js
@@ -3,6 +3,8 @@ import path from 'path'
 const dataFilePath = path.join(__dirname, '../data/demo-data.sl3')
 const pgUrl = process.env.DATABASE_URL
 const connection = pgUrl || { filename: dataFilePath }
+
+console.log('connecting to ' + JSON.stringify(connection))
 export default require('knex')({
   client: pgUrl ? 'pg' : 'sqlite3',
   connection,
