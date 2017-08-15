b90f1b7e6b736b82cb3a294324a227f69a7dcb81
Patrik Weibull <patrik.weibull@gmail.com>
2017-08-15 13:28:33 +0000

    Added various view types

--- seeds/3.view_types.js
+++ seeds/3.view_types.js
@@ -31,17 +31,87 @@ let insertViewTypes = async (datas, knex) => {
 
 exports.seed = async function(knex, Promise) {
   return await insertViewTypes([
+    /* Layouts */
     {
       name: [{lang: 'en', content: 'Overview'}],
+      /* TODO: 2-3 menyer. ev. bildspel, footer-info? */
       schema: {},
       schemaForm: [],
       component: 'Overview',
+    },
+    {
+      /* TODO: ?. Somehow make it possible to specify full-width jumbotrons via shortcode in content */
+      name: [{lang: 'en', content: 'Showcase'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Showcase',
+    },
+    {
+      name: [{lang: 'en', content: 'Documentation'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Documentation',
+    },
+    {
+      name: [{lang: 'en', content: 'Forum'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Forum',
+    },
+    {
+      name: [{lang: 'en', content: 'Blog'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Blog',
     },
     {
       name: [{lang: 'en', content: 'Admin'}],
       schema: {},
       schemaForm: [],
       component: 'Admin',
-    }
+    },
+    /* Types */
+    {
+      name: [{lang: 'en', content: 'Article'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Article',
+    },
+    {
+      name: [{lang: 'en', content: 'Category'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Category',
+    },
+    {
+      name: [{lang: 'en', content: 'Video'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Video',
+    },
+    {
+      name: [{lang: 'en', content: 'Music'}],
+      schema: {},
+      schemaForm: [],
+      component: 'Music',
+    },
+    {
+      name: [{lang: 'en', content: 'FileList'}],
+      schema: {},
+      schemaForm: [],
+      component: 'FileList',
+    },
+    {
+      name: [{lang: 'en', content: 'File'}],
+      schema: {},
+      schemaForm: [],
+      component: 'File',
+    },
+    {
+      name: [{lang: 'en', content: 'User'}],
+      schema: {},
+      schemaForm: [],
+      component: 'User',
+    },
   ], knex);
 };
