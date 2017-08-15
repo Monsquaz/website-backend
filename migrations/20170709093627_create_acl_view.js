d7e09a461cd20a3a529cfee20c4b7390cefd85de
Patrik Weibull <patrik.weibull@gmail.com>
2017-08-15 14:59:21 +0000

    Fixed acl view. Again.

--- migrations/20170709093627_create_acl_view.js
+++ migrations/20170709093627_create_acl_view.js
@@ -1,13 +1,16 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.raw(`CREATE VIEW acl AS
-    SELECT DISTINCT a.id AS action_id, a.name AS action_name, aa.descendant AS administrable_id, COALESCE(uaa.user_id, uu.user_id) AS user_id
+    (SELECT a.id AS action_id, a.name AS action_name, aa.descendant AS administrable_id, uaa.user_id AS user_id
     FROM administrables_administrables AS aa
-    LEFT JOIN users_actions_administrables AS uaa ON uaa.administrable_id=aa.ancestor
-    LEFT JOIN usergroups_actions_administrables AS ugaa ON ugaa.administrable_id=aa.ancestor
-    LEFT JOIN users_usergroups AS uu ON uu.usergroup_id = ugaa.usergroup_id
-    JOIN actions AS a ON (a.id=uaa.action_id OR a.id=ugaa.action_id)
-    WHERE (uaa.user_id IS NOT NULL OR uu.user_id IS NOT NULL)`);
+    JOIN users_actions_administrables AS uaa ON uaa.administrable_id=aa.ancestor
+    JOIN actions AS a ON a.id=uaa.action_id)
+    UNION
+    (SELECT a.id AS action_id, a.name AS action_name, aa.descendant AS administrable_id, uu.user_id AS user_id
+    FROM administrables_administrables AS aa
+    JOIN usergroups_actions_administrables AS ugaa ON ugaa.administrable_id=aa.ancestor
+    JOIN users_usergroups AS uu ON uu.usergroup_id = ugaa.usergroup_id
+    JOIN actions AS a ON a.id=ugaa.action_id)`);
 };
 
 exports.down = function(knex, Promise) {
