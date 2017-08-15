eeef18d148137cf4710d47cad1fe517ff19d23d0
Patrik Weibull <patrik.weibull@gmail.com>
2017-08-15 13:13:18 +0000

    Fixed problem with ACL view. It didn't work for usergroups.

--- migrations/20170709093627_create_acl_view.js
+++ migrations/20170709093627_create_acl_view.js
@@ -1,12 +1,13 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.raw(`CREATE VIEW acl AS
-    SELECT DISTINCT a.id AS action_id, a.name AS action_name, aa.descendant AS administrable_id, u.id AS user_id FROM administrables_administrables AS aa
+    SELECT DISTINCT a.id AS action_id, a.name AS action_name, aa.descendant AS administrable_id, COALESCE(uaa.user_id, uu.user_id) AS user_id
+    FROM administrables_administrables AS aa
     LEFT JOIN users_actions_administrables AS uaa ON uaa.administrable_id=aa.ancestor
-    LEFT JOIN users AS u ON uaa.user_id = u.id
-    LEFT JOIN users_usergroups AS uu ON uu.user_id=u.id
-    LEFT JOIN usergroups_actions_administrables AS ugaa ON ugaa.administrable_id=aa.ancestor AND ugaa.usergroup_id=uu.usergroup_id
-    JOIN actions AS a ON (a.id=uaa.action_id OR a.id=ugaa.action_id)`);
+    LEFT JOIN usergroups_actions_administrables AS ugaa ON ugaa.administrable_id=aa.ancestor
+    LEFT JOIN users_usergroups AS uu ON uu.usergroup_id = ugaa.usergroup_id
+    JOIN actions AS a ON (a.id=uaa.action_id OR a.id=ugaa.action_id)
+    WHERE (uaa.user_id IS NOT NULL OR uu.user_id IS NOT NULL)`);
 };
 
 exports.down = function(knex, Promise) {
